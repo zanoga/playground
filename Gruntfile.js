410893cc67cd1f3e85b8e7fee4f8aee34cf7339c
Artem Mezin <iketari@gmail.com>
2016-02-22 15:04:44 +0300

    rename

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,24 +3,32 @@ module.exports = function(grunt) {
   grunt.initConfig({
     requirejs: {
         options: {
-                baseUrl: '.',
-                // webroot: 'script',
-                config: ['js/require.config.js'],
-                name: 'js/main',
-        		require: 'bower_components/requirejs/require',
-        		almond: 'bower_components/almond/almond',
-                out: 'js/build.js'
-            },
-            dev: {
-        		options: {
-        			build: true
-        		}
+            baseUrl: '.',
+            config: ['js/require.config.js'],
+            name: 'js/main',
+    		require: 'bower_components/requirejs/require',
+    		almond: 'bower_components/almond/almond',
+            out: 'build/main.js'
+        },
+        prod: {
+    		options: {
+    			build: true
+    		}
+        }
+    },
+
+    uglify: {
+        prod: {
+            files: {
+                'build/main.min.js': ['build/main.js']
             }
+        }
     }
   });
 
   grunt.loadNpmTasks('grunt-require');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['requirejs:dev']);
+  grunt.registerTask('default', ['requirejs:prod']);
 
 };
